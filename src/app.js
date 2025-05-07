e19434d79048698e5194f3b06f7787f1f393ab97
rohittt-29 <rm2193352@gmail.com>
2025-05-07 17:24:08 +0530

    exploring get post , delete

--- src/app.js
+++ src/app.js
@@ -4,13 +4,24 @@ const app = express();
 
 
 
-app.use("/test",(req,res)=>{
+app.get("/user",(req, res)=>{
+    res.send("kya bolti public")
+})
+  
+
+app.post("/user",(req, res)=>{
+    res.send("bantai ko lageli bhook")
+})
+
+app.delete("/user",(req, res)=>{
+    res.send("abrakadabra abrakadabra")
+})
+
+
+app.use("/user",(req,res)=>{
     res.send("sorry sorry sorry !")
 })
 
-app.use("/rohityaa",(req,res)=>{
-    res.send("kya bolte server bhai!")
-})
 
 app.listen(9000,()=>{
     console.log("server run ho raha hai bantaiii")
