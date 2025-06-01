79cf0183b57603c6d5ca398c25e8e0aa23130721
rohittt-29 <rm2193352@gmail.com>
2025-06-01 17:15:43 +0530

    using express.router

--- src/app.js
+++ src/app.js
@@ -3,84 +3,23 @@ const express = require('express');
 const connectDB = require("./config/database")
 
 const app = express();
-const User = require("./models/user")
-const {validateSignupData} = require("./utils/validation")
-const bcrypt = require("bcrypt");
 const cookieParser = require("cookie-parser");
-const jwt = require("jsonwebtoken");  
-const {UserAuth} = require("./middlewares/auth")
-
 
 app.use(express.json());
 app.use(cookieParser())
-app.post("/signup", async(req, res)=>{
-    try{ validateSignupData(req)
+
+const authRouter = require("./routes/auth");
+const profileRouter = require("./routes/Profile");
+const requestRouter = require("./routes/request");
+
+app.use("/", authRouter)
+app.use("/", profileRouter)
+app.use("/", requestRouter);
  
 
-    const {firstName, LastName, emailId, password} = req.body
-
-    const passwordHash = await bcrypt.hash(password,10);
-    console.log(passwordHash)
-
-       const user = new User({
-        firstName,
-        LastName,
-        emailId,
-        password: passwordHash,
-       });
-
-    await user.save();
-    res.send("user aagye oyeee");}
-    catch(err){
-        res.status(400).send("Error: " + err.message)
-    }
-   
-});
-
-app.post("/login", async(req,res)=>{
-    try{
-        const {emailId, password}= req.body;
-
-        const user = await User.findOne({emailId: emailId});
-        if(!user){
-            throw new Error("InValid Credentials");
-        }
-        const isPasswordValid = await user.validatepassword(password)
-
-        if(isPasswordValid){
-       const token = await user.getJwt();
 
 
 
-
-        res.cookie("token",token)
-
-            res.send("Login hogaya");
-        }else{
-            throw new Error("InValid Credentials")
-        }
-    }
-      catch(err){
-        res.status(400).send("Error: " + err.message)
-    }
-})
-
-app.get("/profile", UserAuth,async(req,res)=>{
-    try{
-
-    const user = req.user;
-    res.send(user)
-}
-      catch(err){
-        res.status(400).send("Error: " + err.message)
-    }
-});
-
-app.post("/sendRequest",UserAuth, async(req, res)=>{
-const user = req.user;
-    console.log("sending a connection request");
-    res.send(user.firstName + " sending a connection request")
-})
 
 
 
---
+++ src/routes/auth.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const authRouter = express.Router();
+const User = require("../models/user");
+const {validateSignupData} = require("../utils/validation")
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");  
+
+
+authRouter.post("/signup", async(req, res)=>{
+    try{ validateSignupData(req)
+ 
+
+    const {firstName, LastName, emailId, password} = req.body
+
+    const passwordHash = await bcrypt.hash(password,10);
+    console.log(passwordHash)
+
+       const user = new User({
+        firstName,
+        LastName,
+        emailId,
+        password: passwordHash,
+       });
+
+    await user.save();
+    res.send("user aagye oyeee");}
+    catch(err){
+        res.status(400).send("Error: " + err.message)
+    }
+   
+});
+
+authRouter.post("/login", async(req,res)=>{
+    try{
+        const {emailId, password}= req.body;
+
+        const user = await User.findOne({emailId: emailId});
+        if(!user){
+            throw new Error("InValid Credentials");
+        }
+        const isPasswordValid = await user.validatepassword(password)
+
+        if(isPasswordValid){
+       const token = await user.getJwt();
+
+
+
+
+        res.cookie("token",token)
+
+            res.send("Login hogaya");
+        }else{
+            throw new Error("InValid Credentials")
+        }
+    }
+      catch(err){
+        res.status(400).send("Error: " + err.message)
+    }
+});
+
+module.exports = authRouter;
